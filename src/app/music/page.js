d911a045e1e76c40ef7e8f8fee4a4d413ad2ecd9
Karthikcahry23 <lingojikarthikchary@gmail.com>
2025-02-18 17:17:28 +0530

    Fixed missing react-icons module

--- src/app/music/page.js
+++ src/app/music/page.js
@@ -5,6 +5,7 @@ import ReactPlayer from "react-player";
 import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
 
+
 export default function MusicSearch() {
   const [searchTerm, setSearchTerm] = useState("");
   const [videos, setVideos] = useState([]);
